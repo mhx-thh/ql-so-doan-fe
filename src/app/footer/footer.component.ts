d4b08a261457e60abd58da084c7cf76e25a92dcc
Vinh Nguyen <nguyentanvinh2911@gmail.com>
2021-08-12 19:58:55 +0700

    delete footer at admin route

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  headerFooter = null;
 
   ngOnInit(): void {
+    this.router.events
+    .subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.headerFooter = (event.url !== '/admin')
+      }
+    });
   }
 
 }
