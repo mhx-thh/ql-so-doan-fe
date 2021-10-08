3cbc43fe2357b5a1820710fd4f3f042f513dc358
NguyenHao-us <haonguyencp22@gmail.com>
2021-10-08 22:35:07 +0700

    update

--- pages/admin/book.tsx
+++ pages/admin/book.tsx
@@ -1,49 +1,123 @@
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
-import { BookModel } from "lib/models";
+import { BookModel, ClassModel, HistoryModel } from "lib/models";
 
-import AdminLayout from "components/layout/AdminLayout";
 import bookApi from "api/bookApi";
+import configApi from "api/configApi";
 
 import ViewBook from "components/adminPage/ViewBook";
 import { ConvertTime } from "components/ConvertTime";
-import { stringify } from "querystring";
+import AddOrEditHistory from "components/adminPage/AddOrEditHistory";
+import AdminLayout from "components/layout/AdminLayout";
 
 export default function Books() {
-  const [books, setBookse] = useState<Array<BookModel>>(null);
-  const [isOpen, setOpen] = useState(false);
+  const [classes, setClasses] = useState<Array<ClassModel>>([]);
+
   const [book, setBook] = useState<BookModel>(null);
+  const [books, setBooks] = useState<Array<BookModel>>(null);
+  const [open, setOpen] = useState(false);
   const [hideBook, setHideBook] = useState(false);
 
-  async function fetchBookList() {
-    Swal.fire({
-      title: "Loading data",
-      icon: "info",
-      timerProgressBar: true,
-      didOpen: () => {
-        Swal.showLoading();
-      },
-    });
+  const [isEdit, setIsEdit] = useState(false);
+  const [history, setHistory] = useState<HistoryModel>(null);
+  const [histories, setHistories] = useState<Array<HistoryModel>>(null);
+  const [openHistory, setOpenHistory] = useState(false);
+
+  // FetchApi: Books, BookWithSID, Histories
+  async function fetchClassList() {
+    // Swal.fire({
+    //   title: "Loading data",
+    //   icon: "info",
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
     try {
-      const res = await bookApi.getBookList();
+      const res = await configApi.getClassList("");
       const data = res?.data?.data?.result;
-      console.log(data);
-      setBookse(data);
+      setClasses(data);
     } catch (error) {
       console.log(error.message);
     }
-    Swal.close();
+    // Swal.close();
+  }
+  async function fetchBookList() {
+    // Swal.fire({
+    //   title: "Loading data",
+    //   icon: "info",
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
+    try {
+      const res = await bookApi.getBookList();
+      const data = res?.data?.data?.result;
+      setBooks(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+    // Swal.close();
+  }
+  async function fetchHistoryList() {
+    // Swal.fire({
+    //   title: "Loading data",
+    //   icon: "info",
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
+    try {
+      const res = await configApi.getHistoryList();
+      const data = res?.data?.data?.result;
+      setHistories(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+    // Swal.close();
+  }
+  async function fetchClass() {
+    // Swal.fire({
+    //   title: "Loading data",
+    //   icon: "info",
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
+    try {
+      const res = await configApi.getClassList("");
+      const data = res?.data?.data?.result;
+      setClasses(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+    // Swal.close();
   }
 
   useEffect(() => {
-    fetchBookList();
+    async function fetchData() {
+      Swal.fire({
+        title: "Loading data",
+        icon: "info",
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+      await fetchBookList();
+      await fetchClass();
+      await fetchHistoryList();
+      await fetchClassList();
+      Swal.close();
+    }
+    fetchData();
   }, []);
 
-  function onViewClick(index: number) {
-    setBook(books[index]);
-    setOpen(true);
-  }
+  // Function create, delete, updateApi
   async function ApprovalClick(id: string, formValues: BookModel) {
     Swal.fire({
       title: "Đang thực thi",
@@ -60,6 +134,71 @@ export default function Books() {
     }
     Swal.close();
   }
+  async function delBook(id: string) {
+    Swal.fire({
+      title: "Đang thực thi",
+      icon: "info",
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+    try {
+      await bookApi.delBook(id, "");
+    } catch (error) {
+      console.log(error.message);
+    }
+    Swal.close();
+  }
+  async function delHistory(id: string) {
+    Swal.fire({
+      title: "Đang thực thi",
+      icon: "info",
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+    try {
+      await configApi.delHistory(id, "");
+    } catch (error) {
+      console.log(error.message);
+    }
+    Swal.close();
+  }
+  async function editHistory(id: string, formValues: HistoryModel) {
+    // Swal.fire({
+    //   title: "Đang thực thi",
+    //   icon: "info",
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
+    try {
+      await configApi.editHistory(id, formValues, "");
+    } catch (error) {
+      console.log(error.message);
+    }
+    // Swal.close();
+  }
+  async function createHistory(formValues: HistoryModel) {
+    // Swal.fire({
+    //   title: "Đang thực thi",
+    //   icon: "info",
+    //   timerProgressBar: true,
+    //   didOpen: () => {
+    //     Swal.showLoading();
+    //   },
+    // });
+    try {
+      await configApi.newHistory(formValues, "");
+    } catch (error) {
+      console.log(error.message);
+    }
+    // Swal.close();
+  }
+  // Function when Click Button
   function onAprovalClick(index: number) {
     setBook({ ...book, Approval: true });
     try {
@@ -75,23 +214,31 @@ export default function Books() {
       }).then(async (result) => {
         if (result.isConfirmed) {
           await ApprovalClick(books[index]._id, book);
-          fetchBookList();
+          await fetchBookList();
+          Swal.fire({
+            icon: "success",
+            title: `Duyệt thành công`,
+            showConfirmButton: false,
+            timer: 1000,
+          });
         }
       });
     } catch (err) {
       console.log(err);
     }
   }
+  // Todo
   function onRequestClick(index: number) {
     // Điều hướng sang trang Request
   }
-  function onHistoryClick(index: number) {
+  function onViewHistoryClick(index: number) {
     setBook(books[index]);
     setHideBook(true);
   }
+  // Todo
   function onUpdateClick(index: number) {
     Swal.fire({
-      title: "Bạn đã hoàn thành Yêu cầy này",
+      title: "Bạn đã hoàn thành Yêu cầu này",
       text: "Thao tác này giúp bạn đánh dấu là Yêu cầu đã được hoàn thành",
       icon: "info",
       confirmButtonColor: "#d33",
@@ -106,6 +253,10 @@ export default function Books() {
       }
     });
   }
+  function onViewBookClick(index: number) {
+    setBook(books[index]);
+    setOpen(true);
+  }
   function onDelClick(index: number) {
     Swal.fire({
       title: "Bạn có chắc chắn?",
@@ -118,17 +269,106 @@ export default function Books() {
       cancelButtonText: "Hủy",
     }).then(async (result) => {
       if (result.isConfirmed) {
-        // await delRequest(index);
-        // await fetchRequestList();
+        await delBook(books[index]._id);
+        await fetchBookList();
+        Swal.fire({
+          icon: "success",
+          title: `Xoá thành công`,
+          showConfirmButton: false,
+          timer: 1000,
+        });
       }
     });
   }
-  function ClickViewBook() {
+  function onDelHistoryClick(index: number) {
+    try {
+      Swal.fire({
+        title: "Bạn có chắc chắn?",
+        text: "Thao tác này có thế sẽ không quay lại được",
+        icon: "warning",
+        confirmButtonColor: "#d33",
+        confirmButtonText: "Xóa!",
+        showCancelButton: true,
+        cancelButtonColor: "#3085d6",
+        cancelButtonText: "Hủy",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          await delHistory(histories[index]._id);
+          await fetchHistoryList();
+          Swal.fire({
+            icon: "success",
+            title: `${isEdit ? "Chỉnh sửa" : "Tạo"} thành công`,
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        }
+      });
+    } catch (e) {
+      Swal.fire("Something wrong !", "Đã xảy ra lỗi", "error");
+      console.log(e);
+    }
+  }
+  function onAddHistoryClick() {
+    setIsEdit(false);
+    setHistory({
+      Book: book?._id,
+      Class: null,
+      Content: "",
+      Time: undefined,
+    });
+    setOpenHistory(true);
+  }
+  function onUpdateHistoryClick(index: number) {
+    setIsEdit(true);
+    setHistory(histories[index]);
+    setOpenHistory(true);
+  }
+  function onClickViewBook() {
     setHideBook(false);
   }
+  async function onAddOrEditDoneClick() {
+    setOpenHistory(false);
+    try {
+      Swal.fire({
+        title: "Loading data",
+        icon: "info",
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+      if (isEdit) {
+        await editHistory(history?._id, history);
+      } else {
+        await createHistory(history);
+      }
+      await fetchBookList();
+      await fetchHistoryList();
+      Swal.fire({
+        icon: "success",
+        title: `${isEdit ? "Chỉnh sửa" : "Tạo"} thành công`,
+        showConfirmButton: false,
+        timer: 1000,
+      });
+    } catch (e) {
+      Swal.fire("Đã xảy ra lỗi!!", "", "error");
+      console.log(e);
+    }
+  }
+
   return (
     <>
-      {isOpen && <ViewBook setOpen={setOpen} book={book} />}
+      {open && <ViewBook setOpen={setOpen} book={book} />}
+      {openHistory && (
+        <AddOrEditHistory
+          history={history}
+          onAddOrEditDoneClick={onAddOrEditDoneClick}
+          setHistory={setHistory}
+          setOpen={setOpenHistory}
+          isEdit={isEdit}
+          classes={classes}
+        />
+      )}
       <AdminLayout>
         <div className="flex flex-wrap mt-4 box-border">
           <div className="w-full mb-12 px-4">
@@ -208,7 +448,7 @@ export default function Books() {
                                   </button>
                                   <ul className="z-10 bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32">
                                     <button
-                                      onClick={() => onViewClick(i)}
+                                      onClick={() => onViewBookClick(i)}
                                       className="block w-full rounded-sm px-3 py-1 hover:bg-gray-100 hover:font-medium"
                                     >
                                       Xem chi tiết
@@ -237,7 +477,7 @@ export default function Books() {
                                     </button>
                                     {e.Approval && (
                                       <button
-                                        onClick={() => onHistoryClick(i)}
+                                        onClick={() => onViewHistoryClick(i)}
                                         className="block w-full rounded-sm px-3 py-1 hover:bg-gray-100 hover:font-medium"
                                       >
                                         Lịch sử
@@ -271,8 +511,8 @@ export default function Books() {
                   <div className="flex flex-wrap items-center">
                     <div className="flex relative w-full max-w-full flex-grow flex-1">
                       <button
-                        className="rotate-180 flex hover:bg-gray-200 px-2"
-                        onClick={ClickViewBook}
+                        className="rotate-180 flex hover:bg-gray-200 px-2 hover:rounded-xl"
+                        onClick={onClickViewBook}
                       >
                         <p className="-rotate-180 px-2 font-semibold text-lg light text-gray-600">
                           Quay về
@@ -305,12 +545,12 @@ export default function Books() {
                         </svg>
                       </button>
                       <h3 className="font-semibold text-lg light pl-4">
-                        {book.SID} - Lịch sử sổ đoàn
+                        Lịch sử sổ đoàn của {book?.SID} - {book?.Name}
                       </h3>
                     </div>
                     <button
-                      // onClick={onAddClick}
-                      className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                      onClick={onAddHistoryClick}
+                      className="bg-blueGray-600 active:bg-blueGray-600 hover:bg-blueGray-700 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                       type="button"
                     >
                       Tạo lịch sử
@@ -335,44 +575,46 @@ export default function Books() {
                       </tr>
                     </thead>
                     <tbody>
-                      {book.histories?.map((e, i) => {
-                        return (
-                          <tr key={i}>
-                            <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                              <span className="ml-3 font-bold text-blueGray-600">
-                                {ConvertTime(e.Time.toString())}
-                              </span>
-                            </th>
-                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                              {e?.Class?.Name}
-                            </td>
-                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                              {e.Content}
-                            </td>
-                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                              <div className="group inline-block">
-                                <button className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32">
-                                  <i className="fas fa-ellipsis-v group-hover:-rotate-90"></i>
-                                </button>
-                                <ul className="z-10 bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32">
-                                  <button
-                                    onClick={() => onUpdateClick(i)}
-                                    className="block w-full rounded-sm px-3 py-1 hover:bg-gray-100 hover:font-medium"
-                                  >
-                                    Cập nhật
+                      {histories
+                        ?.filter((e) => e.Book === book._id)
+                        .map((e, i) => {
+                          return (
+                            <tr key={i}>
+                              <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                                <span className="ml-3 font-bold text-blueGray-600">
+                                  {ConvertTime(e.Time.toString())}
+                                </span>
+                              </th>
+                              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                {e?.Class?.Name}
+                              </td>
+                              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                {e?.Content}
+                              </td>
+                              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                                <div className="group inline-block">
+                                  <button className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32">
+                                    <i className="fas fa-ellipsis-v group-hover:-rotate-90"></i>
                                   </button>
-                                  <button
-                                    onClick={() => onDelClick(i)}
-                                    className="block w-full rounded-sm px-3 py-1 hover:bg-gray-100 hover:font-medium"
-                                  >
-                                    Xóa
-                                  </button>
-                                </ul>
-                              </div>
-                            </td>
-                          </tr>
-                        );
-                      })}
+                                  <ul className="z-10 bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32">
+                                    <button
+                                      onClick={() => onUpdateHistoryClick(i)}
+                                      className="block w-full rounded-sm px-3 py-1 hover:bg-gray-100 hover:font-medium"
+                                    >
+                                      Cập nhật
+                                    </button>
+                                    <button
+                                      onClick={() => onDelHistoryClick(i)}
+                                      className="block w-full rounded-sm px-3 py-1 hover:bg-gray-100 hover:font-medium"
+                                    >
+                                      Xóa
+                                    </button>
+                                  </ul>
+                                </div>
+                              </td>
+                            </tr>
+                          );
+                        })}
                     </tbody>
                   </table>
                 </div>
